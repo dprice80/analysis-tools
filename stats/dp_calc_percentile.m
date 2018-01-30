83d006dd6d6d792dc923fde9d0b041b6938aca08
dprice80 <dprice80@gmail.com>
2018-01-30 12:59:56 +0000

    Added twotailed option, p vals, and z scores to dp_calc_percentile.m

--- stats/dp_calc_percentile.m
+++ stats/dp_calc_percentile.m
@@ -1,11 +1,49 @@
-function pc = calcpc(nulldist, lzr)
+function [pc, p, z] = dp_calc_percentile(nulldist, x, tail)
+
+if nargin == 2
+    tail = 'onetailed';
+end
 
 N = length(nulldist);
-loc = find(lzr >= sort(nulldist), 1, 'last');
-if isempty(loc)
-    pc = 1/N;
-elseif loc ~= N
-    pc = loc/N;
-else
-    pc = 1-(1/N); % can't have 100th percentiles in here (which cannot be converted to a z score)
+
+pc = zeros(size(x));
+p = pc;
+
+% q = quantile(nulldist, 0.5);
+
+for ii = 1:length(x)
+    
+%     switch tail
+%         case 'onetailed'
+            loc = find(x(ii) >= sort(nulldist), 1, 'last');
+%         case 'twotailed'
+%             if x(ii) < q % need to treat lower and upper half of null dist differently
+%                 loc = find(x(ii) <= sort(nulldist), 1, 'first');
+%             else
+%                 loc = find(x(ii) >= sort(nulldist), 1, 'last');
+%             end
+%     end
+    
+    if isempty(loc)
+        pc(ii) = 1/N;
+    elseif loc ~= N
+        pc(ii) = loc/N;
+    else
+        pc(ii) = 1-(1/N); % can't have 100th percentiles in here (which cannot be converted to a z score)
+    end
+    
+    switch tail
+        case 'twotailed'
+            pc2t = abs(pc(ii)-0.5)*2;
+            % Calculate p value of result
+            p(ii) = 1-(pc2t);
+        case 'onetailed'
+            p(ii) = 1-pc(ii);
+        otherwise
+            error('tail should be either ''onetailed'' or ''twotailed''')
+    end
+end
+
+if exist('norminv', 'file')
+    z = norminv(pc, 0, 1);
 end
--- surrogate/dp_phaserand_surrogate_ex1.m
+++ surrogate/dp_phaserand_surrogate_ex1.m
@@ -10,6 +10,7 @@ scaling2 = 1.4; % how much to scale segment 2
 x1 = randn(1,fs*Lt);
 x2 = randn(1,fs*Lt);
 % Sum together 
+% Need fieldtrip on path
 x12(1,:) = ft_preproc_bandpassfilter([x1 x2], fs, [1 4], 4, 'but', 'onepass', 'reduce'); % filter data using fieldtrip (recommended)
 x12(2,:) = ft_preproc_bandpassfilter([x1 x2], fs, [4 8], 4, 'but', 'onepass', 'reduce'); % filter data using fieldtrip (recommended)
 x12(3,:) = ft_preproc_bandpassfilter([x1 x2], fs, [8 13], 4, 'but', 'onepass', 'reduce'); % filter data using fieldtrip (recommended)
@@ -26,7 +27,7 @@ x12h = abs(hilbert(x12));
 a1 = mean(x12h(1:L/2)); % first half 
 a2 = mean(x12h(L/2+1:L)); % second half
 
-Nrand = 1000;
+Nrand = 10000;
 
 a1r = zeros(1,Nrand);
 a2r = zeros(1,Nrand);
@@ -40,7 +41,8 @@ for ii = 1:Nrand
 end
 
 adr = a2r./a1r;
-%%
+
+%
 close all
 histogram(adr)
 
@@ -48,28 +50,28 @@ ad = a2/a1;
 
 adrs = sort(adr);
 
-% Calculate percentile of real result
-pc = find(adrs >= ad, 1, 'first')/Nrand;
-
-% Need to take care of cases where real value is beyond the simulation.
-if isempty(pc)
-    pc = (Nrand-1)./Nrand;
-end
-
-pc2t = abs(pc-0.5)*2
-
-% Calculate p value of result
-pval2t = 1-(pc2t)
+% Calculate percentile and p value
+[pc, p] = dp_calc_percentile(adrs, ad, 'twotailed');
 
 % Calculate p value thresholds
 % Upper threshold is easy (but we need to make 2 tailed by dividing by two)
 pthresh_upper = quantile(adr, [1-0.05/2 1-0.01/2 1-0.001/2])
 
 % Lower threshold is the inverse of the upper threshold
-pthresh_lower = 1./pthresh_upper
+pthresh_lower = quantile(adr, [0.05/2 0.01/2 0.001/2])
 
+hold on 
+ylim = get(gca, 'YLim');
+th = [pthresh_upper pthresh_lower];
+line([th; th], repmat(ylim,6,1)')
 
+% We can verify that these tally with the calculation of p values 
+% pv should be [0.05 0.01 0.001] in both cases
+% pcv should be [0.9750 0.9950 0.9995] or [0.0250 0.0050 0.0005]
+% z should be +/-[1.9600 2.5758 3.2905]
+[pcv, pv, z] = dp_calc_percentile(adrs, pthresh_upper, 'twotailed')
 
+[pcv, pv, z] = dp_calc_percentile(adrs, pthresh_lower, 'twotailed')
 
 
 
